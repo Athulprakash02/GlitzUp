1293a9fa4c54e8b15245b0c4478eb59d22a89368
Athulprakash02 <athulkichu02@gmail.com>
2023-07-10 00:44:11 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:glitzup/application/core/themes.dart';
 import 'package:glitzup/presentatioon/screens/auth/login_screen.dart';
 
-void main(List<String> args) {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
